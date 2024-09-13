417884de511bb6ecca22022a96a50c72e32246ba
Yashwanth <100037024+yashwanth-smarty@users.noreply.github.com>
2024-09-13 09:39:15 +0530

    updated env variable

--- send.py
+++ send.py
@@ -1,6 +1,6 @@
 import telepot
 
 def sendpicture(message,image):
-    bot = telepot.Bot('6177243905:AAFqwIx5NCPznKv3pXJ8sT-6ibUKVm_VM5o')
-    bot.sendPhoto(5046094109, photo=open(image, 'rb'))
-    bot.sendMessage(5046094109, message)
+    bot = telepot.Bot(TELEGRAM_BOT_TOKEN)
+    bot.sendPhoto(TELEGRAM_CHAT_ID, photo=open(image, 'rb'))
+    bot.sendMessage(TELEGRAM_CHAT_ID, message)
